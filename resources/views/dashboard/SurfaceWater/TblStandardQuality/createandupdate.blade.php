9c8853e6aba08a172d582c11ae995a302488d16d
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-12-17 15:51:46 +0800

    asdf

--- resources/views/dashboard/SurfaceWater/TblStandardQuality/createandupdate.blade.php
+++ resources/views/dashboard/SurfaceWater/TblStandardQuality/createandupdate.blade.php
@@ -41,7 +41,7 @@
                                 <div class="row">
                                     <label class="col-sm-4 col-form-label">TSS Standard</label>
                                     <div class="col-sm-8">
-                                        <input type="number" step="0.01" name="tss_standard" 
+                                        <input type="text"  name="tss_standard" 
                                                value="{{ old('tss_standard', $data->tss_standard) }}" 
                                                class="form-control form-control-sm @error('tss_standard') is-invalid @enderror">
                                         @error('tss_standard') <span class="invalid-feedback">{{ $message }}</span> @enderror
@@ -53,7 +53,7 @@
                                 <div class="row">
                                     <label class="col-sm-4 col-form-label">PH Min Standard</label>
                                     <div class="col-sm-8">
-                                        <input type="number" step="0.01" name="ph_min_standard" 
+                                        <input type="text"  name="ph_min_standard" 
                                                value="{{ old('ph_min_standard', $data->ph_min_standard) }}" 
                                                class="form-control form-control-sm @error('ph_min_standard') is-invalid @enderror">
                                         @error('ph_min_standard') <span class="invalid-feedback">{{ $message }}</span> @enderror
@@ -65,7 +65,7 @@
                                 <div class="row">
                                     <label class="col-sm-4 col-form-label">PH Max Standard</label>
                                     <div class="col-sm-8">
-                                        <input type="number" step="0.01" name="ph_max_standard" 
+                                        <input type="text"  name="ph_max_standard" 
                                                value="{{ old('ph_max_standard', $data->ph_max_standard) }}" 
                                                class="form-control form-control-sm @error('ph_max_standard') is-invalid @enderror">
                                         @error('ph_max_standard') <span class="invalid-feedback">{{ $message }}</span> @enderror
@@ -78,7 +78,7 @@
                                 <div class="row">
                                     <label class="col-sm-4 col-form-label">DO Standard</label>
                                     <div class="col-sm-8">
-                                        <input type="number" step="0.01" name="do_standard" 
+                                        <input type="text"  name="do_standard" 
                                                value="{{ old('do_standard', $data->do_standard) }}" 
                                                class="form-control form-control-sm @error('do_standard') is-invalid @enderror">
                                         @error('do_standard') <span class="invalid-feedback">{{ $message }}</span> @enderror
@@ -91,7 +91,7 @@
                                 <div class="row">
                                     <label class="col-sm-4 col-form-label">Redox Standard</label>
                                     <div class="col-sm-8">
-                                        <input type="number" step="0.01" name="redox_standard" 
+                                        <input type="text"  name="redox_standard" 
                                                value="{{ old('redox_standard', $data->redox_standard) }}" 
                                                class="form-control form-control-sm @error('redox_standard') is-invalid @enderror">
                                         @error('redox_standard') <span class="invalid-feedback">{{ $message }}</span> @enderror
@@ -104,7 +104,7 @@
                                 <div class="row">
                                     <label class="col-sm-4 col-form-label">Conductivity Standard</label>
                                     <div class="col-sm-8">
-                                        <input type="number" step="0.01" name="conductivity_standard" 
+                                        <input type="text"  name="conductivity_standard" 
                                                value="{{ old('conductivity_standard', $data->conductivity_standard) }}" 
                                                class="form-control form-control-sm @error('conductivity_standard') is-invalid @enderror">
                                         @error('conductivity_standard') <span class="invalid-feedback">{{ $message }}</span> @enderror
@@ -117,7 +117,7 @@
                                 <div class="row">
                                     <label class="col-sm-4 col-form-label">Temperatur Standard</label>
                                     <div class="col-sm-8">
-                                        <input type="number" step="0.01" name="temperatur_standard" 
+                                        <input type="text"  name="temperatur_standard" 
                                                value="{{ old('temperatur_standard', $data->temperatur_standard) }}" 
                                                class="form-control form-control-sm @error('temperatur_standard') is-invalid @enderror">
                                         @error('temperatur_standard') <span class="invalid-feedback">{{ $message }}</span> @enderror
